4509d4e1378021d477de7f55f09f1c42fd801575
HampusS <supmahampus@gmail.com>
2018-05-23 15:51:11 +0200

    Added routes and resourced them: product, review, store

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/products', 'ProductController');
+Route::resource('/reviews', 'ReviewController');
+Route::resource('/stores', 'StoreController');
